b5f0801029bf2f08e0f41e1c8e927936e56ecc6a
Swapvp <swapneshpawar1994@gmail.com>
2024-05-11 11:36:31 +0530

    step progress bar

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Step Progress Bar
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import StepProgressBarTest from "./Components/StepProgressBarTest";
+
+function App() {
+  return <StepProgressBarTest />;
+}
+
+export default App;
